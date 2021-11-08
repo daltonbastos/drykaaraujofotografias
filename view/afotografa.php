5d090c9dea138d95ebcf18f63cbaa0c6ececbf3d
Dalton Bastos <daltonBastos@hotmail.com>
2021-11-08 19:17:58 -0300

    past the page afotografa for php

---
+++ view/afotografa.php
@@ -0,0 +1,44 @@
+<section id="bio">
+    <img src="img/dry.jpg" alt="Adrieli Araújo">
+
+    <div>
+        <h2>ADRIELI ARAÚJO</h2>
+        <p>Olá tudo bem?</p>
+        <P>
+            Sou natural de São Paulo mas passei maior parte da vida morando na área rural 
+            em  Remanso Bahia,  tenho 21 anos e curso Artes Visuais na Universidade 
+            Federal do Vale do São Francisco. Desde criança sentia afinidade e paixão 
+            pela arte ainda que de forma indireta e não tivesse consciência disso 
+            deixando me levar pela liberdade de criação e imaginação quando pintava ou 
+            escrevia.  
+        </P>
+        <P>
+            Em 2015 com a minha mudança para a Cidade e contato com as redes sociais 
+            descobri meu amor pela fotografia e decidi que essa era a linguagem 
+            artística que queria me dedicar, em 2018 entrei no curso de artes e desde 
+            então tenho me aprofundado e estudado sobre a área tirando fotos de 
+            paisagens, pessoas, objetos e tudo aquilo que consigo enxergar com o meu 
+            olhar. Hoje estou me especializando em Retratos e está sendo uma experiência 
+            fantástica pois amo o que faço. 
+        </P>
+        <P>
+            Com o meu olhar busco registrar o que há de mais belo em cada pessoa 
+            fotografada, gosto de ver a alegria nas pessoas ao se olharem com outros 
+            olhos pelos quais não se vêem no dia a dia  através da minha arte.
+        </P>
+    </div>            
+</section>
+
+<section class="social-card">
+    <img src="img/dry_profile.jpg" alt="Adrieli Araújo">
+
+    <div>
+        <p>Redes sociais</p>
+        <h3>ADRIELI ARAÚJO</h3>
+        <div class="row">
+            <a href="https://l.instagram.com/?u=https%3A%2F%2Fbit.ly%2F2TGlZlk&e=ATMIJjj9n18E3EAyBAS33lK-sQFO4zZfxRBQduhaosLrX_NchQwGIzsUNh5nCMq38K4gpiFOki8s-PuYqr-IQw&s=1" target="_blank"><i class="fa fa-whatsapp"></i></a>
+            <a href="https://www.instagram.com/drykraujofotografias/" target="_blank"><i class="fa fa-instagram"></i></a>
+            <a href="mailto:drykraujo@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a>
+        </div>
+    </div>
+</section>
